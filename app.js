416ff482393938c424b141c94d2a01aea2afc666
saravana <ksaravanan11111@gmail.com>
2021-01-03 10:23:04 +0530

    second commit

--- app.js
+++ app.js
@@ -9,7 +9,36 @@
 
         function toBuyController(buyBoughtServer) {
             var toBuy = this;
-            toBuy.items = [{
+            toBuy.item = [];
+            toBuy.name = "";
+            toBuy.quantity = "";
+            // toBuy.moveItem = function(itemIndex) {
+            //     buyBoughtServer.moveItem(itemIndex, toBuy.items);
+            // }
+            toBuy.item = buyBoughtServer.items;
+            toBuy.moveItem1 = function(itemIndex) {
+                buyBoughtServer.move1(itemIndex, toBuy.item)
+            }
+            toBuy.add = function() {
+                buyBoughtServer.add(toBuy.name, toBuy.quantity);
+            }
+
+
+        };
+
+        alreadyBoughtController.$inject = ['buyBoughtServer'];
+
+        function alreadyBoughtController(buyBoughtServer) {
+            var alreadyBought = this;
+            alreadyBought.items = buyBoughtServer.item;
+            alreadyBought.remove = function(itemIndex) {
+                buyBoughtServer.delete(itemIndex, alreadyBought.items);
+            }
+        };
+
+        function buyBoughtServer() {
+            var buyBought = this;
+            buyBought.items = [{
                     name: 'litres of milk',
                     quantity: 3
                 },
@@ -34,34 +63,48 @@
                     quantity: 4
                 }
             ];
-            toBuy.moveItem = function(itemIndex) {
-                buyBoughtServer.moveItem(itemIndex, toBuy.items);
-            }
-
-
-        };
-
-        alreadyBoughtController.$inject = ['buyBoughtServer'];
-
-        function alreadyBoughtController(buyBoughtServer) {
-            var alreadyBought = this;
-            alreadyBought.items = buyBoughtServer.items;
-        };
-
-        function buyBoughtServer() {
-            var buyBought = this;
-            buyBought.items = [];
-            buyBought.moveItem = function(itemIndex, arrayOfitems) {
+            buyBought.item = [];
+            // buyBought.moveItem = function(itemIndex, arrayOfitems) {
+            //     var items = {
+            //         name: '',
+            //         quantity: ''
+            //     };
+            //     items.name = arrayOfitems[itemIndex].name;
+            //     items.quantity = arrayOfitems[itemIndex].quantity;
+            //     arrayOfitems.splice(itemIndex, 1);
+            //     buyBought.item.push(items);
+            // };
+            buyBought.delete = function(itemIndex, listItems) {
                 var item = {
                     name: '',
                     quantity: ''
-                }
+
+                };
+                item.name = listItems[itemIndex].name;
+                item.quantity = listItems[itemIndex].quantity;
+                listItems.splice(itemIndex, 1);
+                buyBought.items.push(item);
+
+
+            };
+            buyBought.move1 = function(itemIndex, arrayOfitems) {
+                var item = {
+                    name: '',
+                    quantity: ''
+                };
                 item.name = arrayOfitems[itemIndex].name;
                 item.quantity = arrayOfitems[itemIndex].quantity;
                 arrayOfitems.splice(itemIndex, 1);
-                buyBought.items.push(item);
+                buyBought.item.push(item);
             };
+            buyBought.add = function(name, quantity) {
+                var items = {
+                    name: name,
+                    quantity: quantity
+                };
+                buyBought.items.push(items)
+            }
 
-        };
+        }
     }
 )();
